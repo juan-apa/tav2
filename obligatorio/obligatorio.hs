4d55acae832e7d0931135f07139a68e0c8563855
Juan Aparicio <apariciojuan30@gmail.com>
2018-11-17 12:11:56 -0300

    yaEstaRegistrado - terminado

--- obligatorio/obligatorio.hs
+++ obligatorio/obligatorio.hs
@@ -2,8 +2,16 @@
 --data Curso = codigo Integer| docente Asistente| nombreCurso String| fecha String| asistentes [Asistente] deriving Show
 
 --data Persona = Asistente String String String | Docente String String String
-data Asistente = Asistente String String String deriving (Show,Eq)
 
+-- ============= ASISTENTE =============
+data Asistente = Asistente String String String deriving (Show)
+
+instance Eq Asistente where
+    (Asistente nom1 ap1 ced1) == (Asistente nom2 ap2 ced2) = ((nom1 == nom2) && (ap1 == ap2) && (ced1 == ced2))
+
+getCedulaAS :: Asistente -> String
+getCedulaAS (Asistente _ _ cedula) = cedula
+-- ============= DOCENTE =============
 data Docente = Docente String String String deriving (Show)
 
 instance Eq Docente where
@@ -22,6 +30,9 @@ getCodCurso (Curso cod _ _ _) = cod
 
 getDocente :: Curso -> Docente
 getDocente (Curso _ docente _ _) = docente
+
+getAsistentes :: Curso -> [Asistente]
+getAsistentes (Curso _ _ _ lAsist) = lAsist
 
 instance Ord Curso where
     (Curso cod _ _ _) > (Curso cod2 _ _ _) = (cod > cod2)
@@ -51,6 +62,19 @@ listCursos :: Cursos a -> [a]
 listCursos CursosVacio = []
 listCursos (Nodo a left right) = (listCursos left) ++ [a] ++ (listCursos right)
 
+-- Devuelve true si existe un curso con el mismo codigo
+existeCurso :: Integer -> Cursos Curso -> Bool
+existeCurso cod CursosVacio = False
+existeCurso cod (Nodo a left right) 
+            | cod == (getCodCurso a) = True
+            | cod < (getCodCurso a) = existeCurso cod left
+            | cod > (getCodCurso a) = existeCurso cod right
+
+obtenerCurso :: Integer -> Cursos Curso -> Curso
+obtenerCurso cod (Nodo cur left right)
+    |cod == (getCodCurso cur) = cur
+    |cod < (getCodCurso cur) = obtenerCurso cod left
+    |cod > (getCodCurso cur) = obtenerCurso cod right
 -- Obtiene los docentes de la lista de cursos
 -- parametros: Lista Curso
 listDocentesCursos :: [Curso] -> [Docente]
@@ -61,11 +85,21 @@ listDocentesCursos l = map (getDocente) l
 listCedulasDocentes :: [Docente] -> [String]
 listCedulasDocentes l = map (getCedula) l
 
+-- Obtiene las cedulas de los asistentes
+-- parametros: Lista Asistente
+listCedulasAsistentes :: [Asistente] -> [String]
+listCedulasAsistentes l = map getCedulaAS l
+
 -- Devuelve si el docente dicto algun curso
 -- parametros: ABB de cursos; Cedula Docente
 yaEsDocente :: Cursos Curso -> String -> Bool
 yaEsDocente abb ced = (length (filter (== ced) (listCedulasDocentes (listDocentesCursos (listCursos abb)) )) > 0)
 
+-- Devuelve true si la persona con la cedula ya esta registrada al curso
+yaEstaRegistrado :: Cursos Curso -> String -> Integer -> Bool
+yaEstaRegistrado abb ced cod
+    |existeCurso cod abb = (length (filter (== ced) (listCedulasAsistentes (getAsistentes (obtenerCurso cod abb)))) > 0)
+    |otherwise = False
 
 asist = Asistente "Nombre" "Apellido" "Cedula" 
 docente = Docente "Nombre" "Apellido" "Cedula" 
